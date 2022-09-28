73f888dc6fb788eae82a2ab29d9fb331bec0af17
denisdelaki <denisdelaki@gmail.com>
2022-09-28 13:44:07 +0300

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    #validate name such that the name cannot be empty or falsy
+    validates :name, presence: true, uniqueness: true
+    #validate the name such that  the name is case sensitive 
+    validates :phone_number, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
-class Post < ApplicationRecord
+class Post < ApplicationRecord 
+    #vlidate the title such that the title cannot be empty or falsy 
+    validates :title, presence: true
+    #validate the length of the content to be at least 250 
+   validates :content, length: {minimum: 250}
+    #validate the length of the summary to be at most 250 
+   validates :summary, length: {maximum: 250}
+    #validate the category 
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+   #Allow title to be "True Facts"
+   validate :is_clickbait 
+   def  is_clickbait
+      if title == "True Facts"
+        errors.add(:title, "Title not permitted")
+    end
+end
 end
